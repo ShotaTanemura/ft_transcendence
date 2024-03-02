bc94793d133433fc8716eb0fad4afd9a88bb3788
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-03-02 19:44:26 +0900

    barが動かない！

--- websocket/pong/consumers.py
+++ websocket/pong/consumers.py
@@ -5,16 +5,17 @@ import asyncio
 
 class MyConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.is_connected = True
         await self.accept()
 
     async def disconnect(self, close_code):
-        pass
+        self.websocket.close()
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         # TODO: receiveが2回目以降呼ばれない。reason:while loop to keep updating the game
         update_player_position(text_data_json['key'])
-        while True:
+        while self.is_connected:
             updatePong()
             game_data = {
                 "ball": ball,
@@ -23,5 +24,11 @@ class MyConsumer(AsyncWebsocketConsumer):
                 "canvas": canvas,
             }
             json_data = json.dumps(game_data)
-            await self.send(text_data=json_data)
-            await asyncio.sleep(0.05)
+            try:
+                await self.send(text_data=json_data)
+            except websocket.exceptions.ConnectionClosedError:
+                self.is_connected = False  # 安全のため切断状態をフラグに反映
+                self.websocket.close()
+                print("Connection closed, stopping the loop")
+                break
+            await asyncio.sleep(0.02)
