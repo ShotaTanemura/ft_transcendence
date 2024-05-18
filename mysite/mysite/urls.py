eacc7bce8165d619bfe08d0ccdd9c4f2bf01fab8
Sota Okuno <103186087+soooku@users.noreply.github.com>
2024-05-18 07:34:35 +0000

    refactor: mysite -> sampleSPA

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for sampleSPA project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
