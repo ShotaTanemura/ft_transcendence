192384b67768a190c6712c18b8d9e24b2cd910d8
massahito <110250805+massahito@users.noreply.github.com>
2024-05-22 17:19:25 +0900

    allows GameStartButton to submit user name

--- pong-game/src/components/Home.jsx
+++ pong-game/src/components/Home.jsx
@@ -2,6 +2,7 @@ import {React, useState, useContext} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {PlayersContext} from '../App.jsx'
 import {UserNameInput} from './user-name-input.jsx'
+import { GameStartButton } from './game-start-button.jsx';
 
 export function Home() {
   const { playersInfo, setPlayersInfo} = useContext(PlayersContext);
@@ -12,9 +13,6 @@ export function Home() {
 	setPlayersInfo({
 	  //TODO 同じニックネームをつけないなどのバリデーションが必要。
 	  player1: document.getElementById('player1').value,
-	  player2: document.getElementById('player2').value,
-	  player3: document.getElementById('player3').value,
-	  player4: document.getElementById('player4').value
 	})
 	navigation("/tournament");
 	return ;                         	
@@ -26,7 +24,7 @@ export function Home() {
 	  	<h1>Welcome to Pong Game!</h1>
 	  	<form id="RegisterPlayers" onSubmit={handlesubmit}>
 		  <UserNameInput id={1}/>
-      	  <button type="submit">Game Start!</button>
+		  <GameStartButton/>
     	</form>
 	  </div>
     </>
