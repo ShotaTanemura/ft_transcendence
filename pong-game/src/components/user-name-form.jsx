6b66d5232902aa92a1413851351c45ac66acc597
massahito <110250805+massahito@users.noreply.github.com>
2024-05-23 14:45:19 +0900

    allows UserNameForm to add and remove user

--- pong-game/src/components/user-name-form.jsx
+++ pong-game/src/components/user-name-form.jsx
@@ -1,17 +1,24 @@
-import {React, Component} from 'react'
+import {React, Component, useState} from 'react'
 import {UserNameInput} from './user-name-input'
 import {GameStartButton} from './game-start-button'
 
-export class UserNameForm extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-	  	  <form onSubmit={this.props.handlesubmit}>
-		    <UserNameInput id={1}/>
-		    <GameStartButton/>
-      	  </form>
-        );
-    }
+export function UserNameForm(props) {
+    const [inputBlocks, setInputBlocks] = useState([1, 2]);
+	function addUserNameInput() {
+		setInputBlocks(prevBlocks => [...prevBlocks, prevBlocks.length + 1]);
+	}
+	function removeUserNameInput() {
+		setInputBlocks(prevBlocks => prevBlocks.slice(0, prevBlocks.length - 1));
+	}
+
+    return (
+	  <form onSubmit={props.handlesubmit}>
+        {inputBlocks.map((id) => (
+          <UserNameInput key={id} id={id}/>
+        ))}
+	    <GameStartButton/>
+        <button type="button" onClick={addUserNameInput}>+click</button>
+        <button type="button" onClick={removeUserNameInput}>-click</button>
+      </form>
+    );
 }
