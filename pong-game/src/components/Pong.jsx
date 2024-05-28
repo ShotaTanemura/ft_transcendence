219d9ebd0244d47b99beb945be0d68ed29508ebc
ShotaTanemura <shota0711tane@gmaail.com>
2024-05-28 07:18:04 +0000

    Make getNextGamePairName function obvious

--- pong-game/src/components/Pong.jsx
+++ pong-game/src/components/Pong.jsx
@@ -2,7 +2,7 @@ import { React } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export const Pong = () => {
-    const getNextGamePairName = (rounds) => {
+    function getNextGamePairName(rounds) {
         const flattenedGames = rounds.flatMap(round => round);
         const nextGame = flattenedGames.find(game => game.top.winner && game.bottom.winner);
         return nextGame ? [nextGame.top.name, nextGame.bottom.name] : null
