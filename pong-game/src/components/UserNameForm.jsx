e0d650c4ff9a39488b62304c4069fc4c0614038f
massahito <110250805+massahito@users.noreply.github.com>
2024-05-29 14:26:18 +0900

    add vite config

---
+++ pong-game/src/components/UserNameForm.jsx
@@ -0,0 +1,29 @@
+import {React, useContext, useState} from 'react'
+import {UserNameInput} from './UserNameInput.jsx'
+import {GameStartButton} from './GameStartButton.jsx'
+import { PlayersContext } from '../App.jsx';
+
+export function UserNameForm(props) {
+    const [inputBlocks, setInputBlocks] = useState([1, 2]);
+    const { playersInfo, setPlayersInfo} = useContext(PlayersContext);
+	function addUserNameInput() {
+		setInputBlocks(prevBlocks => [...prevBlocks, prevBlocks.length + 1]);
+	}
+	function removeUserNameInput() {
+		setInputBlocks(prevBlocks => prevBlocks.slice(0, prevBlocks.length - 1));
+	}
+  function onInputChange(id, value) {
+    setPlayersInfo(players => {players[id] = {name: value, is_advancing: true}; return players;})
+  }
+
+    return (
+	  <form onSubmit={props.handlesubmit}>
+        {inputBlocks.map((id) => (
+          <UserNameInput key={id} id={id} onInputChange={onInputChange}/>
+        ))}
+	    <GameStartButton/>
+        <button type="button" onClick={addUserNameInput}>+click</button>
+        <button type="button" onClick={removeUserNameInput}>-click</button>
+      </form>
+    );
+}
