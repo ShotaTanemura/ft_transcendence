a9f85d143151e4d6910ace786c51587f12392440
massahito <110250805+massahito@users.noreply.github.com>
2024-06-01 16:58:57 +0900

    import core files into main.js

--- vanilla-pong-game/main.js
+++ vanilla-pong-game/main.js
@@ -2,6 +2,8 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import './srcs/core/router.js'
+import './srcs/core/component.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
