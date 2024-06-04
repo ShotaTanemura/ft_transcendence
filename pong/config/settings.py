eb0de113efb39f82cbf3f2446b83418b982893fe
Sota Okuno <103186087+soooku@users.noreply.github.com>
2024-06-04 20:16:01 +0900

    fix: CSRFを有効化

--- pong/config/settings.py
+++ pong/config/settings.py
@@ -44,8 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 一時的にCSRFを無効化
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
