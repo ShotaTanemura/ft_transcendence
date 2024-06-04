9b08915f2c2cd4eb47db524d536bd95156e8f6c0
Sota Okuno <103186087+soooku@users.noreply.github.com>
2024-06-04 20:21:03 +0900

    feat: データベースにユーザーを登録するメソッドを作成

--- pong/pong/views.py
+++ pong/pong/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
+from django.http.response import JsonResponse
+from .models import UserManager
+from .models import User
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def register(request):
+
+	if request.method != 'POST':
+		return JsonResponse({
+			'message': 'Method is not allowed',
+			'status': 'invalidParams'
+		}, status=400)
+
+	data = json.loads(request.body)
+
+	if 'name' not in data or 'email' not in data or 'password' not in data:
+		return JsonResponse({
+			'message': 'Invalid parameters',
+			'status': 'invalidParams'
+		}, status=400)
+
+	if User.objects.filter(name=data['name']).exists():
+		return JsonResponse({
+			'message': 'User already exists',
+			'status': 'registerConflict'
+		}, status=409)
+
+	user = User.objects.create_user(name=data['name'], email=data['email'], password=data['password'])
+
+	return JsonResponse({
+		'uuid': user.uuid
+	}, status=201)
