529703ed550a2957fe05f42874f0461ce5f8d8f6
Sota Okuno <103186087+soooku@users.noreply.github.com>
2024-06-06 16:00:07 +0900

    feat: ユーザー登録のテストを追加

--- pong/pong/tests.py
+++ pong/pong/tests.py
@@ -1,3 +1,76 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import User
 
-# Create your tests here.
+class UserViewsTest(TestCase):
+	def test_register_normal(self):
+		data = {
+			'name': 'ユーザー名',
+			'email': 'example@email.com',
+			'password': 'p4s$W0rd'
+		}
+		response = self.client.post(reverse('pong:register'), data=data, content_type='application/json')
+		user = User.objects.filter(name=data['name']).first()
+		self.assertEqual(response.json()['uuid'], str(user.uuid))
+		self.assertTrue(user.check_password(data['password']))
+		self.assertEqual(response.status_code, 201)
+
+	def test_register_not_allowed_method(self):
+		response = self.client.get(reverse('pong:register'))
+		self.assertEqual(response.json()['message'], 'Method is not allowed')
+		self.assertEqual(response.json()['status'], 'invalidParams')
+		self.assertEqual(response.status_code, 400)
+
+	def test_register_no_name(self):
+		data = {
+			'email': 'example@email.com',
+			'password': 'p4s$W0rd'
+		}
+		response = self.client.post(reverse('pong:register'), data=data, content_type='application/json')
+		self.assertEqual(response.json()['message'], 'Invalid parameters')
+		self.assertEqual(response.json()['status'], 'invalidParams')
+		self.assertEqual(response.status_code, 400)
+
+	def test_register_no_email(self):
+		data = {
+			'name': 'ユーザー名',
+			'password': 'p4s$W0rd'
+		}
+		response = self.client.post(reverse('pong:register'), data=data, content_type='application/json')
+		self.assertEqual(response.json()['message'], 'Invalid parameters')
+		self.assertEqual(response.json()['status'], 'invalidParams')
+		self.assertEqual(response.status_code, 400)
+
+	def test_register_no_password(self):
+		data = {
+			'name': 'ユーザー名',
+			'email': 'example@email.com',
+		}
+		response = self.client.post(reverse('pong:register'), data=data, content_type='application/json')
+		self.assertEqual(response.json()['message'], 'Invalid parameters')
+		self.assertEqual(response.json()['status'], 'invalidParams')
+		self.assertEqual(response.status_code, 400)
+
+	def test_register_user_already_exists(self):
+		User.objects.create_user(name='ユーザー名', email='example@email1.com', password='p4s$W0rd')
+		data = {
+			'name': 'ユーザー名',
+			'email': 'example2@email.com',
+			'password': 'p4s$W0rd'
+		}
+		response = self.client.post(reverse('pong:register'), data=data, content_type='application/json')
+		self.assertEqual(response.json()['message'], 'User already exists')
+		self.assertEqual(response.json()['status'], 'registerConflict')
+		self.assertEqual(response.status_code, 409)
+
+	def test_register_email_already_exists(self):
+		User.objects.create_user(name='ユーザー名1', email='example@email.com', password='p4s$W0rd')
+		data = {
+			'name': 'ユーザー名2',
+			'email': 'example@email.com',
+			'password': 'p4s$W0rd'
+		}
+		response = self.client.post(reverse('pong:register'), data=data, content_type='application/json')
+		self.assertEqual(response.json()['message'], 'User already exists')
+		self.assertEqual(response.json()['status'], 'registerConflict')
+		self.assertEqual(response.status_code, 409)
