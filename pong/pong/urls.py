348d08fc257937ce33ae439941a8821fe64873f9
Sota Okuno <103186087+soooku@users.noreply.github.com>
2024-06-09 16:03:12 +0900

    refactor: refreshのurlを変更

--- pong/pong/urls.py
+++ pong/pong/urls.py
@@ -5,5 +5,5 @@ app_name = 'pong'
 urlpatterns = [
     path('api/v1/auth/register', views.register, name='register'),
     path('api/v1/auth/token', views.create_token, name='token'),
-    path('api/v1/auth/refresh', views.refresh_token, name='refresh'),
+    path('api/v1/auth/token/refresh', views.refresh_token, name='refresh'),
 ]
