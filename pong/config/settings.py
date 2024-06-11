a6b2e02ea1f4acd245098399f89e740615c2d07a
Sota Okuno <103186087+soooku@users.noreply.github.com>
2024-06-11 19:38:38 +0900

    feat: setting.pyにmiddlewareのpathを追加

--- pong/config/settings.py
+++ pong/config/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pong.middleware.auth.JWTAuthenticationMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
