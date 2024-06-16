1edc9d807c6d0524b1eecdbc675d39f2035e853e
Sota Okuno <103186087+soooku@users.noreply.github.com>
2024-06-16 18:51:46 +0900

    feat: ログイン画面の作成

---
+++ pong/pong/static/pong/components/Hoge.js
@@ -0,0 +1,53 @@
+import { Component } from "../core/component.js";
+
+export class Hoge extends Component {
+	constructor(router, params, state) {
+		super(router, params, state);
+		this.findElement("form.sign-up-form").onsubmit = this.onSubmit;
+	}
+
+	onSubmit = async (event) => {
+		event.preventDefault();
+		let username = this.findElement("#username").value;
+		let password = this.findElement("#password").value;
+		let repeatPassword = this.findElement("#repeat-password").value;
+		let email = this.findElement("#email").value;
+		if (password !== repeatPassword) {
+			alert("password and repeat password are not same.");
+			return;
+		}
+		const data = {"name": username, "password": password, "email": email};
+		const response = await fetch("/pong/api/v1/auth/register", {
+			method: "POST",
+			mode: "cors",
+			cache: "no-cache",
+			credentials: "same-origin",
+			headers: {
+			  "Content-Type": "application/json",
+			},
+			redirect: "follow",
+			referrerPolicy: "no-referrer",
+			body: JSON.stringify(data),
+		  });
+		console.log(response.json());
+	}
+
+	get html() {
+		return `
+			<div>
+				<h1>HOGE</h1>
+				<form class="sign-up-form">
+					<label for="username">Username</label>
+				   <input type=text placeholder="user name" id="username" name="username" required></input><br/>
+				   <label for="password">Password</label>
+				   <input type=password placeholder="enter password" id="password" name="password" required></input><br/>
+				   <label for="repeat-password">Repeat Password</label>
+				   <input type=password placeholder="repeat password" id="repeat-password" name="repeat-password" required></input><br/>
+				   <label for="email">email</label>
+				   <input type=email placeholder="email"id="email" name="email" required></input><br/>
+				   <button class="form-submit" type=submit>sign up</button>
+				</form>
+			</div>
+		`;
+	}
+}
