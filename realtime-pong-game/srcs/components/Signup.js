9dc4a94d5b7a6c61c36a22f30fde276fcdb09d46
massahito <110250805+massahito@users.noreply.github.com>
2024-06-17 03:13:09 +0900

    add: add Signup Component

--- realtime-pong-game/srcs/components/Signup.js
+++ realtime-pong-game/srcs/components/Signup.js
@@ -1,5 +1,77 @@
+//TODO passwordやusernameのvalidationの要件がDjangoの要件と一致していない
+//TODO エラーページやログイン画面への遷移を書いていない。
+//TODO passwordをhash化してから送っていない。
 import { Component } from "../core/component";
+import "./Signup.css"
 
 export class Signup extends Component {
+    constructor(router, params, state) {
+        super(router, params, state);
+        this.findElement("form.signup-form").onsubmit = this.onSignup;
+    }
 
+    fetchPOSTData = async (JSONData) => {
+        try {
+            const response = await fetch("/pong/api/v1/auth/register", {
+                method: "POST",
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSONData
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                throw Error(data.status);
+            }
+        } catch(error) {
+            //Go To Error Page !
+        }
+    }
+
+    onSignup = (event) => {
+        event.preventDefault();
+        if (event.target.password.value != event.target.confirm_password.value){
+            alert("Passwords do not match");
+            return ;
+        }
+
+        const signupJSON = JSON.stringify({
+            "name": event.target.username.value,
+            "email": event.target.email.value,
+            "password": event.target.password.value
+        });
+        this.fetchPOSTData(signupJSON);
+        //Go to Login Page!
+    }
+
+    get html() {
+        return (`
+            <h1>WAAAAAOUH!! Welcome to Pong Game!</h1>
+            <h1>Signup</h1>
+            <br/>
+            <form class="signup-form">
+                <ul>
+                    <li class="username">
+                        <label for="username">username: </label>
+                        <input id="username" name="username" placeholder="username" type="text" max="20" required/>
+                    </li>
+                    <li class="email">
+                        <label for="email">email: </label>
+                        <input id="email" name="email" placeholder="email" type="email" max="320" required/>
+                    </li>
+                    <li class="password">
+                        <label for="password">password: </label>
+                        <input id="password" name="password" placeholder="password" type="password" max="64" required/>
+                    </li>
+                    <li class= "confirm-password">
+                        <label for="confirm_password">confirm password: </label>
+                        <input id="confirm_password" name="comfirm_password" placeholder="confirm_password" type="password" max="64" required/>
+                    <li>
+                    <button>submit</button>
+                <ul/>
+            </form>
+        `);
+    }
 }
+
+export default Signup;
--- realtime-pong-game/srcs/main.js
+++ realtime-pong-game/srcs/main.js
@@ -1,12 +1,13 @@
 import './style.css'
 import { Router } from './core/router.js'
-import {Home} from './components/Home.js'
-import {Room} from './components/Room.js'
+import { Home } from './components/Home.js'
+import { Room } from './components/Room.js'
+import { Signup } from './components/Signup.js';
 
 let router = new Router(document.getElementById("app"), [
 		{
 			path: "/",
-			component: Home,
+			component: Signup,
 			state: {},
 		},
 		{
