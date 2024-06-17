035709ff50be8053a06e1248755662bc1a30f2d9
ShotaTanemura <shota0711tane@gmaail.com>
2024-06-17 13:35:17 +0900

    Define verify_token as a view of api/v1/auth/token/verify

--- pong/pong/urls.py
+++ pong/pong/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('api/v1/auth/register', auth.register, name='register'),
     path('api/v1/auth/token', auth.create_token, name='token'),
     path('api/v1/auth/token/refresh', auth.refresh_token, name='refresh'),
+    path('api/v1/auth/token/verify', auth.verify_token, name='verify'),
 ]
