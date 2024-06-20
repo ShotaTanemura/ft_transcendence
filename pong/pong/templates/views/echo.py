516431ad8660fd0d0f019404f67119271947f9f9
Sota Okuno <103186087+soooku@users.noreply.github.com>
2024-06-20 12:29:00 +0900

    refactor: 不要ファイルを削除

--- pong/pong/templates/views/echo.py
+++
@@ -1,9 +0,0 @@
-from django.http.response import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from pong.middleware.auth import jwt_exempt
-from django.shortcuts import render
-
-@jwt_exempt
-@csrf_exempt
-def echo(request):
-	return render(request, 'pong/index.html')
