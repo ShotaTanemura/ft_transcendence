69830b2da7fb8d4eed4ae412c57c5b8e8c76f6b3
massahito <110250805+massahito@users.noreply.github.com>
2024-07-07 14:14:32 +0900

    fix: change app name to realtime_pong_game

--- pong/config/settings.py
+++ pong/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pong',
-    'realtime',
+    'realtime_pong_game',
 ]
 
 MIDDLEWARE = [
--- pong/realtime_pong_game/apps.py
+++ pong/realtime_pong_game/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RealtimeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'realtime'
+    name = 'realtime_pong_game'
