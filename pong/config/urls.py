f161bdb06d89ae5afd6756fc3474952552ff79e5
Sota Okuno <soooku.0806@gmail.com>
2024-07-20 16:12:00 +0900

    refactor: formatterかける

--- pong/config/urls.py
+++ pong/config/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, re_path, include
 from pong.views.index import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('pong/', include('pong.urls')),
-    re_path(r'^.*$', index, name='index'),
+    path("admin/", admin.site.urls),
+    path("pong/", include("pong.urls")),
+    re_path(r"^.*$", index, name="index"),
 ]
--- pong/pong/apps.py
+++ pong/pong/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PongConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pong'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pong"
