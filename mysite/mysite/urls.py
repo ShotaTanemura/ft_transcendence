2af3434cd365779bc8bddb10c46a0fcb6a1fff88
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-07-21 16:25:52 +0900

    tutorial:01

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for remote project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# mysite/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("chat/", include("chat.urls")),
+    path("admin/", admin.site.urls),
 ]
