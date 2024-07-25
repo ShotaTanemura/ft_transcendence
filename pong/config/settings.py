061e3219ca729a9355b811146d3bb03c6d4ee7d9
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-07-25 20:28:13 +0900

    インデックスビューを追加

--- pong/config/settings.py
+++ pong/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pong',
+    'typing_game',
 ]
 
 MIDDLEWARE = [
--- pong/typing_game/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TypingGameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'typing_game'
