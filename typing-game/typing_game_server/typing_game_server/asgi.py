21ec537987967274d784d4ccb08de553f26a4f36
Subaru Sasaki <subaru.sasaki@i-zuna.co.jp>
2024-08-11 17:06:23 +0900

    channelsを追加

--- typing-game/typing_game_server/typing_game_server/asgi.py
+++ typing-game/typing_game_server/typing_game_server/asgi.py
@@ -12,7 +12,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
-from . import consumers
+from game import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typing_game_server.settings')
 
