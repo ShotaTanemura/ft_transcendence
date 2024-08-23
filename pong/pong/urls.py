06758b5ae396511a1d93d94b6f6bbad6ea14ba8b
ShotaTanemura <shota0711tane@gmaail.com>
2024-08-23 17:20:18 +0900

    api/v1/auth/two-factor/register activate 2fa of a user and publish a provisioning uri

--- pong/pong/urls.py
+++ pong/pong/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import users
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import auth, oauth
+from .views import auth, oauth, two_factor
 
 app_name = "pong"
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
     path("api/v1/users/<uuid:uuid>", users.user, name="user"),
     path("api/v1/users/<uuid:uuid>/icon", users.user_icon, name="user_icon"),
     path("api/v1/auth/token/revoke", auth.revoke_token, name="revoke"),
+    path("api/v1/auth/two-factor/register", two_factor.register, name="two_factor"),
     path("oauth/42/signup", oauth.oauth_42_signup, name="oauth42_signup"),
     path("oauth/42/signin", oauth.oauth_42_signin, name="oauth42_signin"),
     path("oauth/callback/42", oauth.callback_42, name="callback42"),
