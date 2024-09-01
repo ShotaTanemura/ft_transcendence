a64b2513b4d3f3d89af25f0a61d1862cb529b906
libknt <101494707+libknt@users.noreply.github.com>
2024-09-01 15:33:08 +0900

    fix: docker compose app entrypointの修正

--- .devcontainer/requirements/Django/entrypoint.sh
+++ .devcontainer/requirements/Django/entrypoint.sh
@@ -10,7 +10,8 @@ python manage.py collectstatic --no-input
 python create_superuser.py
 
 if [ $DEBUG = 1 ]; then
-    exec python manage.py runserver 0.0.0.0:8000
+    # exec python manage.py runserver 0.0.0.0:8000
+    exec sleep infinityo
 else
     exec uvicorn config.asgi:application --host 0.0.0.0 --port 8000
 fi
