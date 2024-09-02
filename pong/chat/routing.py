9bbbc140ba67be4e66d051d77481b7574cfe77fb
libknt <101494707+libknt@users.noreply.github.com>
2024-09-02 19:17:42 +0900

    refactor: rename consumers

--- pong/chat/routing.py
+++ pong/chat/routing.py
@@ -1,7 +1,7 @@
 # chat/routing.py
 from django.urls import re_path
-from . import consumers
+from . import chat_consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>[\w-]+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>[\w-]+)/$", chat_consumers.ChatConsumer.as_asgi()),
 ]
