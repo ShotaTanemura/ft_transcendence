5124f71fc69f5ed5c37cab6bb8c07bdd9c3d87af
libknt <101494707+libknt@users.noreply.github.com>
2024-09-03 22:16:47 +0900

    add; roomの取得

--- pong/chat/room_consumers.py
+++ pong/chat/room_consumers.py
@@ -4,13 +4,22 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
 from .models import Rooms, Messages
+from django.contrib.auth.models import AnonymousUser
 from logging import getLogger
 
 logger = getLogger(__name__)
 
+def serialize_rooms(room):
+    return {"uuid": str(room.uuid), "name": room.name}
+
 class RoomConsumer(WebsocketConsumer):
     def connect(self):
         self.room_group_name = "room_notifications"
+        self.user = self.scope["user"]
+        if self.user == AnonymousUser():
+            logger.info("Anonymous user not allowed")
+            self.close()
+            return
         try:
             async_to_sync(self.channel_layer.group_add)(
                 self.room_group_name, self.channel_name
@@ -18,6 +27,22 @@ class RoomConsumer(WebsocketConsumer):
             self.accept()
         except Exception as e:
             logger.error(f"Error during initial message sending: {str(e)}")
+            self.close()
+        print(self.user)
+        try:
+            self.send_initial_messages()
+        except Exception as e:
+            logger.info(f"Error during initial message sending: {e}")
+            self.close()
+
+    def send_initial_messages(self):
+        try:
+            rooms = Rooms.objects.filter(userrooms__user_id_id=self.user.uuid)
+            logger.info(f"Retrieved rooms: {rooms}")
+            response_rooms = [serialize_rooms(room) for room in rooms]
+            self.send(text_data=json.dumps({"rooms": response_rooms}))
+        except Rooms.DoesNotExist:
+            logger.info("Room does not exist")
             self.close()
         
 
