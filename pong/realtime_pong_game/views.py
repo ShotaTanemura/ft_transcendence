2bce8b0cb1081568932e11b26fef4ad1c980a361
massahito <110250805+massahito@users.noreply.github.com>
2024-09-07 16:52:54 +0900

    refs #130 match-resultを取得するAPIの追加
    開発途中につき仕様はopenapiは未記入
    テストも未作成
    クライアント側の処理も未作成

---
+++ pong/realtime_pong_game/views.py
@@ -0,0 +1,37 @@
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from pong.middleware.auth import jwt_exempt
+from pong.models import User
+from django.db.models import Q
+from .models import TournamentInfo, MatchInfo
+
+@csrf_exempt
+def get_user_match_result(request, uuid):
+    if request.method != "GET":
+        return JsonResponse(
+            {"message": "Method is not allowed", "status": "invalidParams"}, status=400
+        )
+
+    user = User.objects.filter(uuid=uuid).first()
+
+    if not user:
+        return JsonResponse(
+            {"message": "User not found", "status": "userNotFound"}, status=404
+        )
+
+    user_last_10_match_results = MatchInfo.objects.filter(Q(player1=user) | Q(player2=user)).order_by('-created_at')[:10]
+
+    user_match_results_json = {}
+
+    for index, user_match_result in enumerate(user_last_10_match_results):
+        user_match_results_json[index] = {
+                "player1": user_match_result.player1.name, \
+                "player2": user_match_result.player2.name, \
+                "player1_score": user_match_result.player1_score, \
+                "player2_score": user_match_result.player2_score,
+            } 
+
+    return JsonResponse(
+        user_match_results_json,
+        status=200,
+    )
