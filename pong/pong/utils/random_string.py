e283eba8778091b4b9820c0ddea6a3187213b49a
ShotaTanemura <shota0711tane@gmaail.com>
2024-09-13 18:42:42 +0900

    generate_base32_encoded_random_string generates randome base32 encoded string

--- pong/pong/utils/random_string.py
+++ pong/pong/utils/random_string.py
@@ -1,16 +1,8 @@
-import string
-import random
 import base64
+import os
 
+def generate_random_bytes(length=16):
+    return os.urandom(length)
 
-def generate_raondom_string(length=12):
-    return "".join(
-        random.SystemRandom().choice(string.ascii_uppercase + string.digits)
-        for _ in range(length)
-    )
-
-
-def generate_base32_encoded_raondom_string(length=12):
-    return base64.b32encode(bytearray(generate_raondom_string(length), "ascii")).decode(
-        "utf-8"
-    )
+def generate_base32_encoded_random_string(length=16):
+    return base64.b32encode(generate_random_bytes(length)).decode('utf-8').strip('=')
