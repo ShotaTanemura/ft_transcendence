85586b8703407d70b355e2d856e6f0de2fddd2b6
libknt <101494707+libknt@users.noreply.github.com>
2024-09-14 13:55:03 +0900

    refactor

--- pong/config/asgi.py
+++ pong/config/asgi.py
@@ -27,22 +27,6 @@ import chat.routing
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-import chat.routing
-
-# import chat.routing
-
-# application = ProtocolTypeRouter(
-#     {
-#         "http": django_asgi_app,
-#         "websocket": AllowedHostsOriginValidator(
-#             AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
-#         ),
-#     }
-# )
-
 
 application = ProtocolTypeRouter(
     {
