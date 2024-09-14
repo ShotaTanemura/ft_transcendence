ebf512fc4d77e27b8dba716282fd7f97f1a0fde0
ShotaTanemura <shota0711tane@gmaail.com>
2024-09-14 18:48:36 +0900

    route.js does not block /totp

--- pong/pong/static/pong/core/router.js
+++ pong/pong/static/pong/core/router.js
@@ -45,7 +45,7 @@ export class Router {
       route = this.searchRouteFromPath("/error");
     }
 
-    if (path !== "/signin" && path !== "/signup") {
+    if (path !== "/signin" && path !== "/signup" && path !== "/totp") {
       await this.verifyAndRefreshToken().catch((error) => {
         console.log(error);
         path = "/signin";
