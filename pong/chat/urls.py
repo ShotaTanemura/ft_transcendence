277de54832edbb4e5a0ce086c44eb07a33d081c6
libknt <101494707+libknt@users.noreply.github.com>
2024-09-14 12:20:53 +0900

    refactor: 必要のないapiの削除

--- pong/chat/urls.py
+++ pong/chat/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from .views import test
-from .views.rooms import rooms, unjoined, detail
+from .views import health
 
 app_name = "chat"
 urlpatterns = [
-    path("api/v1/test", test.test, name="test"),
-    path("api/v1/rooms", rooms.handle_rooms, name="handle_rooms"),
-    path("api/v1/rooms/unjoined", unjoined.handle_unjoined_rooms, name="unjoined"),
-    path("api/v1/rooms/<str:room_id>/messages", detail.handle_detail, name="detail"),
+    path("api/v1/health", health.health, name="health"),
 ]
