4e423a6a16a859229eef2dbc8fc5e3a979ef2c6b
massahito <110250805+massahito@users.noreply.github.com>
2024-09-30 13:38:23 +0900

    feat refs #158 add frontend

--- pong/pong/static/pong/api/api.js
+++ pong/pong/static/pong/api/api.js
@@ -53,3 +53,101 @@ export async function getUsersDataFromName(name) {
     return null;
   }
 }
+
+export async function getFriendsData() {
+  try {
+    const response = await fetch(`/pong/api/v1/friends/friends`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    const data = await response.json();
+    if (!response.ok || !data || !data.friends) {
+      throw new Error(data.message || "Failed to fetch user data");
+    }
+    return data.friends;
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return null;
+  }
+}
+
+export async function getRequestedFriendsData() {
+  try {
+    const response = await fetch(`/pong/api/v1/friends/friends/requested`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    const data = await response.json();
+    if (!response.ok || !data || !data["requested-friends"]) {
+      throw new Error(data.message || "Failed to fetch user data");
+    }
+    return data["requested-friends"];
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return null;
+  }
+}
+
+export async function getPendingFriendsData() {
+  try {
+    const response = await fetch(`/pong/api/v1/friends/friends/pending`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    const data = await response.json();
+    if (!response.ok || !data || !data["pending-friends"]) {
+      throw new Error(data.message || "Failed to fetch user data");
+    }
+    return data["pending-friends"];
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return null;
+  }
+}
+
+export async function sendFriendRequest(name) {
+  const send_data = {
+    "requested-user-name": name,
+  };
+  try {
+    const response = await fetch(`/pong/api/v1/friends/request/send`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(send_data),
+    });
+    const data = await response.json();
+    if (!response.ok || !data) {
+      throw new Error(data.message || "Failed to send friend request data");
+    }
+    return true;
+  } catch (error) {
+    console.error("Error send friend request data:", error);
+    return false;
+  }
+}
+
+export async function approveFriendRequest(name) {
+  const send_data = {
+    "request-user-name": name,
+  };
+  try {
+    const response = await fetch(`/pong/api/v1/friends/request/approve`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(send_data),
+    });
+    const data = await response.json();
+    if (!response.ok || !data) {
+      throw new Error(data.message || "Failed to send friend request data");
+    }
+    return true;
+  } catch (error) {
+    console.error("Error send friend request data:", error);
+    return false;
+  }
+}
--- pong/pong/static/pong/components/Friend.js
+++ pong/pong/static/pong/components/Friend.js
@@ -1,15 +1,24 @@
 import { Component } from "../core/component.js";
 import { Header } from "./Header.js";
+import {
+  getFriendsData,
+  getRequestedFriendsData,
+  getPendingFriendsData,
+  approveFriendRequest,
+} from "../api/api.js";
 
 export class Friend extends Component {
   constructor(router, params, state) {
     super(router, params, state);
   }
 
-  afterPageLoaded = () => {
+  afterPageLoaded = async () => {
     this.headerComponent = new Header(this.router, this.params, this.state);
     this.element.parentElement.prepend(this.headerComponent.element);
     this.headerComponent.afterPageLoaded();
+    this.createFriendList();
+    this.createRequestedFriendList();
+    this.createPendingFriendList();
     document.getElementById("button-to-move-home-page").onclick =
       this.goHomePage;
   };
@@ -21,59 +30,144 @@ export class Friend extends Component {
   goHomePage = () => {
     this.goNextPage("/");
   };
-  createSearchedUsersList = async (usersData) => {
-    if (!usersData) {
-      const userNotFoundElement = Object.assign(document.createElement("p"), {
+
+  createFriendList = async () => {
+    const friendsData = await getFriendsData();
+    if (!friendsData) {
+      const friendNotFoundElement = Object.assign(document.createElement("p"), {
         className: "p-3 text-secondary",
       });
-      userNotFoundElement.innerHTML = "Sorry. User Not Found.";
-      document.getElementById("search-users-title").after(userNotFoundElement);
+      friendNotFoundElement.innerHTML = "Sorry. Friend Not Found.";
+      document.getElementById("friend-list-box").after(friendNotFoundElement);
       return;
     }
-    const searchedUsersDiv = Object.assign(document.createElement("div"), {
+    const friendsDiv = Object.assign(document.createElement("div"), {
       className: "p-3",
     });
-    usersData.forEach(async (userData) => {
-      searchedUsersDiv.appendChild(
-        await this.createSearchedUserElement(userData),
-      );
+    console.log(friendsData);
+    friendsData.forEach(async (friendData) => {
+      friendsDiv.appendChild(await this.createFriendElement(friendData));
     });
-    document.getElementById("searched-users-box").appendChild(searchedUsersDiv);
+    document.getElementById("friend-list-box").appendChild(friendsDiv);
   };
 
-  createSearchedUserElement = async (userData) => {
-    const userElement = document.createElement("div");
-    userElement.innerHTML = `
+  createFriendElement = async (friendData) => {
+    const friendElement = document.createElement("div");
+    friendElement.innerHTML = `
         <div class="card m-5 p-2 bg-light border">
           <div class="card-body text-start d-flex align-items-center gap-5">
-            <img size="32" height="128" width="128" data-view-component="true" class="avatar rounded-circle me-5" src=${userData.icon}>
-            <span class="display-4 fw-bold">${userData.name}</span>
+            <img size="32" height="128" width="128" data-view-component="true" class="avatar rounded-circle me-5" src=${friendData.icon}>
+            <span class="display-4 fw-bold">${friendData.name}</span>
           </div>
         </div>
       `;
-    return userElement;
+    return friendElement;
+  };
+
+  createRequestedFriendList = async () => {
+    const friendsData = await getRequestedFriendsData();
+    if (!friendsData) {
+      const friendNotFoundElement = Object.assign(document.createElement("p"), {
+        className: "p-3 text-secondary",
+      });
+      friendNotFoundElement.innerHTML = "No Friend request.";
+      document
+        .getElementById("requested-friend-list-box")
+        .after(friendNotFoundElement);
+      return;
+    }
+    const friendsDiv = Object.assign(document.createElement("div"), {
+      className: "p-3",
+    });
+    console.log(friendsData);
+    friendsData.forEach(async (friendData) => {
+      friendsDiv.appendChild(
+        await this.createRequestedFriendElement(friendData),
+      );
+    });
+    document
+      .getElementById("requested-friend-list-box")
+      .appendChild(friendsDiv);
+  };
+
+  createRequestedFriendElement = async (friendData) => {
+    const requestedFriendElement = document.createElement("form");
+    requestedFriendElement.innerHTML = `
+        <div class="card m-5 p-2 bg-light border">
+          <div class="card-body text-start d-flex align-items-center gap-5">
+            <img size="32" height="128" width="128" data-view-component="true" class="avatar rounded-circle me-5" src=${friendData.icon}>
+            <span class="display-4 fw-bold">${friendData.name}</span>
+            <button id="send-friend-request-to-${friendData.name}" class="btn btn-success ms-auto gap-5">accept friend request</button>
+          </div>
+        </div>
+      `;
+    requestedFriendElement.onsubmit = async (event) => {
+      event.preventDefault();
+      console.log("ok");
+      const button_id = event.submitter.id;
+      const buttonElement = document.getElementById(button_id);
+      if (await approveFriendRequest(friendData.name)) {
+        const parentElement = buttonElement.parentNode;
+        parentElement.removeChild(buttonElement);
+      } else {
+        alert("error!");
+      }
+    };
+    return requestedFriendElement;
+  };
+
+  createPendingFriendList = async () => {
+    const friendsData = await getPendingFriendsData();
+    if (!friendsData) {
+      const friendNotFoundElement = Object.assign(document.createElement("p"), {
+        className: "p-3 text-secondary",
+      });
+      friendNotFoundElement.innerHTML = "No pending friend request.";
+      document
+        .getElementById("pending-friend-list-box")
+        .after(friendNotFoundElement);
+      return;
+    }
+    const friendsDiv = Object.assign(document.createElement("div"), {
+      className: "p-3",
+    });
+    friendsData.forEach(async (friendData) => {
+      friendsDiv.appendChild(await this.createPendingFriendElement(friendData));
+    });
+    document.getElementById("pending-friend-list-box").appendChild(friendsDiv);
+  };
+
+  createPendingFriendElement = async (friendData) => {
+    const requestedFriendElement = document.createElement("div");
+    requestedFriendElement.innerHTML = `
+        <div class="card m-5 p-2 bg-light border">
+          <div class="card-body text-start d-flex align-items-center gap-5">
+            <img size="32" height="128" width="128" data-view-component="true" class="avatar rounded-circle me-5" src=${friendData.icon}>
+            <span class="display-4 fw-bold">${friendData.name}</span>
+            <button id="pedding-friend-${friendData.name}" class="btn btn-secondary ms-auto gap-5">pending..</button>
+          </div>
+        </div>
+      `;
+    return requestedFriendElement;
   };
 
   get html() {
     return `
       <main class="text-center p-5">
         <h1 id="friends-header">Friends</h1>
-        <div class="friend-list-container container border mb-2">
+        <div class="friend-list-container container mb-2">
             <h2 id="friend-list-header">Friend List</h1>
             <div id="friend-list-box" class="d-grid gap-3">
-                <p>no friend..</p>
             </div>
         </div>
-        <div class="friend-request-list-container container border mb-2">
-            <h2 id="friend-request-list-header">Requested</h2>
-            <div id="friend-request-list-box" class="d-grid gap-3">
-                <p>no friend request.</p>
+        <div class="requested-friend-list-container container mb-2">
+            <h2 id="requested-friend-list-header">Requested</h2>
+            <div id="requested-friend-list-box" class="d-grid gap-3">
             </div>
         </div>
-        <div class="pending-friend-list-container container border mb-2">
+        <div class="pending-friend-list-container container mb-2">
             <h2 id="pending-friend-list-header">Pending</h2>
             <div id="pending-friend-list-box" class="d-grid gap-3">
-                <p>no pending friend.</p>
             </div>
         </div>
         <button id="button-to-move-home-page" class="btn btn-primary">back</button>
