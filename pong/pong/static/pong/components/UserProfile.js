05ec2124fb3c14b4918457f780cf72b99f5a7ef7
libknt <101494707+libknt@users.noreply.github.com>
2024-10-03 23:06:59 +0900

    add: user profile

---
+++ pong/pong/static/pong/components/UserProfile.js
@@ -0,0 +1,55 @@
+import { Component } from "../core/component.js";
+import { Header } from "./Header.js";
+import { getUsersDataFromName } from "../api/api.js";
+
+export class UserProfile extends Component {
+  constructor(router, params, state) {
+    super(router, params, state);
+    this.parameters = params;
+    if (this.parameters.user_name) {
+      this.loadUserProfile(this.parameters.user_name);
+    }
+  }
+
+  afterPageLoaded() {
+    this.headerComponent = new Header(this.router, this.params, this.state);
+    this.element.parentElement.prepend(this.headerComponent.element);
+    this.headerComponent.afterPageLoaded();
+  }
+
+  beforePageUnload() {
+    this.element.parentElement.removeChild(this.headerComponent.element);
+  }
+
+  async loadUserProfile(user_name) {
+    try {
+      const users = await getUsersDataFromName(user_name);
+      if (!users) {
+        throw new Error("User not found");
+      }
+      if (users[0]) {
+        this.updateProfileUI(users[0]);
+      }
+    } catch (error) {
+      console.error("Failed to load user profile:", error);
+      window.alert("Failed to load user profile");
+      this.router.goNextPage("/");
+    }
+  }
+
+  updateProfileUI(user) {
+    this.findElement("#username").textContent = user.name;
+    this.findElement("#user-icon").src = user.icon;
+  }
+
+  get html() {
+    return `
+        <div class="profile-card">
+            <h1>プロフィールページ</h1>
+            <img id="user-icon">
+            <p><strong>Username:</strong> <span id="username"></span></p>
+            <br>
+        </div>
+        `;
+  }
+}
