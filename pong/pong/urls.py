856e68f62e2f678cc5f31bf3fe0784134ffe16b6
ShotaTanemura <shota0711tane@gmail.com>
2024-10-10 17:29:42 +0900

    Add API endpoint for two-factor authentication registration

--- pong/pong/urls.py
+++ pong/pong/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("api/v1/auth/token/refresh", auth.refresh_token, name="refresh"),
     path("api/v1/auth/token/verify", auth.verify_token, name="verify"),
     path("api/v1/auth/two-factor/provisioning", two_factor.provisioning, name="tfa_provisioning"),
+    path("api/v1/auth/two-factor/register", two_factor.register, name="tfa_register"),
     path("api/v1/users/<uuid:uuid>", users.user, name="user"),
     path("api/v1/users/<uuid:uuid>/icon", users.user_icon, name="user_icon"),
     path("api/v1/users/others/<str:name>", users.other_user, name="other_user"),
