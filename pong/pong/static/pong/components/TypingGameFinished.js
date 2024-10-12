c0823c8a05bb762daa4f6c9edc6856b37d7aaaab
libknt <101494707+libknt@users.noreply.github.com>
2024-10-12 18:23:59 +0900

    fix: bug

--- pong/pong/static/pong/components/TypingGameFinished.js
+++ pong/pong/static/pong/components/TypingGameFinished.js
@@ -1,10 +1,8 @@
 import { Component } from "../core/component.js";
-import { Load } from "./Load.js";
 import { Header } from "./Header.js";
 
 export class TypingGameFinished extends Component {
   constructor(router, parameters, state) {
-    new Load(router, parameters, state).onload();
     super(router, parameters, state);
     this.findElement("button.go-back-to-game-home").onclick = this.onClick;
   }
