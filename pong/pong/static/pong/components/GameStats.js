b79f1ad3f6e2db4f5124870cefd1a5cfff023f5c
Subaru Sasaki <subaru.sasaki@i-zuna.co.jp>
2024-10-19 19:13:39 +0900

    feat: そのユーザーの試合の勝敗を表示

--- pong/pong/static/pong/components/GameStats.js
+++ pong/pong/static/pong/components/GameStats.js
@@ -13,10 +13,10 @@ export class GameStats extends Component {
     this.element.parentElement.prepend(this.headerComponent.element);
     this.headerComponent.afterPageLoaded();
     this.createPongGameMatchResultTable(
-      await this.getMatchResultsData("/ponggame/api/v1/match-result/"),
+      await GameStats.getMatchResultsData("/ponggame/api/v1/match-result/"),
     );
     this.createTypingGameMatchResultTable(
-      await this.getMatchResultsData("/typinggame/api/v1/match-result/"),
+      await GameStats.getMatchResultsData("/typinggame/api/v1/match-result/"),
     );
   };
 
@@ -32,7 +32,7 @@ export class GameStats extends Component {
     this.goNextPage("/");
   };
 
-  getUserName = async () => {
+  static getUserName = async () => {
     try {
       const userUuid = await getUuid();
       if (!userUuid) {
@@ -50,8 +50,8 @@ export class GameStats extends Component {
     }
   };
 
-  getMatchResultsData = async (apiEndpoint) => {
-    const userName = await this.getUserName();
+  static getMatchResultsData = async (apiEndpoint) => {
+    const userName = await GameStats.getUserName();
     if (!userName) {
       return;
     }
--- pong/pong/static/pong/components/Profile.js
+++ pong/pong/static/pong/components/Profile.js
@@ -1,13 +1,17 @@
 import { Component } from "../core/component.js";
 import { Header } from "./Header.js";
 import { getUuid, getUserFromUuid } from "../api/api.js";
+import { GameStats } from "./GameStats.js"; // GameStatsをインポート
 
 export class Profile extends Component {
   constructor(router, params, state) {
     super(router, params, state);
     this.loadUserProfile();
+    this.loadUserGameResults();
     this.findElement("button.edit-profile-button").onclick = this.goEditProfile;
     this.findElement("button.edit-2fa-button").onclick = this.goEdit2FA;
+    this.TYPINGGAME = "TypingGame";
+    this.PONGGAME = "PongGame";
   }
 
   afterPageLoaded() {
@@ -26,11 +30,11 @@ export class Profile extends Component {
       if (!uuid) {
         throw new Error("UUID not found");
       }
-      const user = await getUserFromUuid(uuid);
-      if (!user) {
+      this.user = await getUserFromUuid(uuid);
+      if (!this.user) {
         throw new Error("User not found");
       }
-      this.updateProfileUI(user);
+      this.updateProfileUI();
     } catch (error) {
       console.error("Failed to load user profile:", error);
       window.alert("Failed to load user profile");
@@ -38,11 +42,77 @@ export class Profile extends Component {
     }
   }
 
-  updateProfileUI(user) {
-    this.findElement("#username").textContent = user.name;
-    this.findElement("#email").textContent = user.email;
-    this.findElement("#user-icon").src = user.icon;
-    console.log(user.icon);
+  async loadUserGameResults() {
+    const pongGameResults = await GameStats.getMatchResultsData(
+      "/ponggame/api/v1/match-result/",
+    );
+    const typingGameResults = await GameStats.getMatchResultsData(
+      "/typinggame/api/v1/match-result/",
+    );
+
+    this.createGameMatchResultTable(pongGameResults, this.PONGGAME);
+    this.createGameMatchResultTable(typingGameResults, this.TYPINGGAME);
+  }
+
+  createGameMatchResultTable = (matchResultData, gameType) => {
+    if (!matchResultData) {
+      return;
+    }
+
+    const tableElement = Object.assign(document.createElement("table"), {
+      className: "table",
+    });
+    const theadElement = document.createElement("thead");
+    const tbodyElement = document.createElement("tbody");
+
+    theadElement.innerHTML = `<tr>
+      <th scope="col">Win or Loss</th>
+      <th scope="col">Opponent</th>
+    </tr>`;
+
+    matchResultData.forEach((matchResult) => {
+      const trElement = document.createElement("tr");
+      let winOrLoss;
+      let opponent;
+
+      if (gameType === this.PONGGAME) {
+        const winner =
+          matchResult.contents.player1_score >
+          matchResult.contents.player2_score
+            ? matchResult.contents.player1
+            : matchResult.contents.player2;
+        winOrLoss = winner === this.user.name ? "Win" : "Loss";
+      } else if (gameType === this.TYPINGGAME) {
+        winOrLoss =
+          matchResult.contents.winner === this.user.name ? "Win" : "Loss";
+      }
+      opponent =
+        matchResult.contents.player1 === this.user.name
+          ? matchResult.contents.player2
+          : matchResult.contents.player1;
+
+      trElement.innerHTML = `
+        <td>${winOrLoss}</td>
+        <td>${opponent}</td>
+      `;
+      tbodyElement.appendChild(trElement);
+    });
+
+    tableElement.appendChild(theadElement);
+    tableElement.appendChild(tbodyElement);
+
+    const tableContainer =
+      gameType === "PongGame"
+        ? this.findElement("div.ponggame-result-table")
+        : this.findElement("div.typinggame-result-table");
+
+    tableContainer.appendChild(tableElement);
+  };
+
+  updateProfileUI() {
+    this.findElement("#username").textContent = this.user.name;
+    this.findElement("#email").textContent = this.user.email;
+    this.findElement("#user-icon").src = this.user.icon;
   }
 
   goEditProfile = () => {
@@ -55,17 +125,26 @@ export class Profile extends Component {
 
   get html() {
     return `
-        <div class="profile-card">
-            <h1>プロフィールページ</h1>
-            <img id="user-icon" height="256" width="256">
-            <br>
-            <br>
-            <p><strong>Username:</strong> <span id="username"></span></p>
-            <p><strong>E-mail:</strong> <span id="email"></span></p>
-            <br>
-            <button class="edit-profile-button">プロフィールを変更する</button>
-            <button class="edit-2fa-button">二要素認証の設定を変更する</button>
+      <div class="profile-card">
+        <h1>プロフィールページ</h1>
+        <img id="user-icon" height="256" width="256">
+        <br>
+        <br>
+        <p>Username: <span id="username"></span></p>
+        <p>E-mail: <span id="email"></span></p>
+        <br>
+        <div class="ponggame-result-table">
+        <h2>Game Stats</h2>
+        <p>PongGame</p>
         </div>
-        `;
+        <div class="typinggame-result-table">
+        <p>TypingGame</p>
+        </div>
+        <button class="edit-profile-button">プロフィールを変更する</button>
+        <br>
+        <br>
+        <button class="edit-2fa-button">二要素認証の設定を変更する</button>
+      </div>
+      `;
   }
 }
