ba7805e94bff40a83d63e0934c503e6ed20598e6
massahito <110250805+massahito@users.noreply.github.com>
2024-10-22 18:41:57 +0900

    fix: fix port issue

--- requirements/Nginx/certificate/create_certificate.sh
+++ requirements/Nginx/certificate/create_certificate.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -aes-256-cbc -out privkey.pem
 openssl req -new -key privkey.pem -out csr.pem
 openssl x509 -req -in csr.pem -signkey privkey.pem -days 90 -out crt.pem
